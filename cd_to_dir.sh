295a95c58d251d38f8f3c43b4d21fd796205bb6b
Fazle Rabbi <fazlerbbi37@@users.noreply.github.com>
2017-01-04 13:36:16 +0600

    added cd_to_dir.sh and pendrive_backup.sh

---
+++ cd_to_dir.sh
@@ -0,0 +1,9 @@
+#author: fr37
+
+#!/bin/bash
+
+#cd_to_projects_dir.sh
+
+#the purpose of the script is to change terminal path to a specific directory
+
+cd path/to/
---
+++ pendrive_backup.sh
@@ -0,0 +1,36 @@
+#author: fr37
+
+#!/bin/bash
+
+#shell scritp name
+
+#the purpose of the script is to backup pendrive data to local drive
+
+#hard code source and destination
+source=
+destination=
+
+#show desktop notifications
+DISPLAY=:0.0 /usr/bin/notify-send "Plugin Pendrive within next 30 seconds for backup"
+
+#sleep for 10 seconds
+sleep 10
+
+#show desktop notifications
+DISPLAY=:0.0 /usr/bin/notify-send "20 seconds..."
+
+#sleep for 10 seconds
+sleep 10
+
+#show desktop notifications
+DISPLAY=:0.0 /usr/bin/notify-send "10 seconds.."
+
+#sleep for 10 seconds
+sleep 10
+
+#rsync with source and destination
+rsync -r $source $destination
+
+#show desktop notifications (confirmation)
+DISPLAY=:0.0 /usr/bin/notify-send "Pendrive Synced ✔"
+
