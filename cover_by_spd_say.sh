812e9eeaa9f40664ae6620147e7efdce8ce38773
Fazle Rabbi <fazlerbbi37@@users.noreply.github.com>
2017-01-11 20:35:21 +0600

    modified cover_by_spd_say.sh

--- cover_by_spd_say.sh
+++ cover_by_spd_say.sh
@@ -1,14 +1,18 @@
 #author: fazlerabbi37
 
-#shell scritp name
+#cover_by_spd_say.sh
 
-#the purpose of the shell script
+#the purpose of the shell script is to read a text file with spd-say command of terminal
 
 #!/bin/bash
 
+#read one line form the file passed as argument and save it line variable 
 while IFS='' read -r line || [[ -n "$line" ]] 
 do
+    #echo that line
     echo $line
+    #say the line with spd-say
     spd-say "$line"
+    #sleep for 3 seconds
     sleep 3
 done < "$1"
