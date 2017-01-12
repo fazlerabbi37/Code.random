a9a20161a253b8e13a615084caa1f2d521f0ab3b
Fazle Rabbi <fazlerbbi37@@users.noreply.github.com>
2017-01-12 14:51:53 +0600

    bug in book_format.java; doesn't work for numbers that are not devisable by 4

--- Java/book_format.java
+++ Java/book_format.java
@@ -1,3 +1,4 @@
+//doesn't WORK !!!
 //author: fazlerabbi37
 //this is a program that will print a document with normal printer in such a way that they will be able to be bind as book
 //dedicated to S M Nafiur Rahman. The man who can pass his whole life without doing nothing but reading books.
