5fbce77b789212c78cf5b3b875f8244c697f378d
Fazle Rabbi <fazlerbbi37@users.noreply.github.com>
2017-01-23 21:37:13 +0600

    changed some messege and alart on pendrive_backup.sh

--- pendrive_backup.sh
+++ pendrive_backup.sh
@@ -2,34 +2,43 @@
 
 #!/bin/bash
 
-#shell scritp name
+#pendrive_backup.sh
 
 #the purpose of the script is to backup pendrive data to local drive
 
 #hard code source and destination
-source=
-destination=
+source=/media/fazlerabbi37/01741380059/
+destination=/home/fazlerabbi37/fr37/Backups/PD/s_d_c_b
 
-#show desktop notifications
+#say and show desktop notifications
 DISPLAY=:0.0 /usr/bin/notify-send "Plugin Pendrive within next 30 seconds for backup"
+spd-say "Plugin Pendrive within next 30 seconds for backup"
 
 #sleep for 10 seconds
 sleep 10
 
-#show desktop notifications
+#say and show desktop notifications
 DISPLAY=:0.0 /usr/bin/notify-send "20 seconds..."
+spd-say "20 seconds..."
 
 #sleep for 10 seconds
 sleep 10
 
-#show desktop notifications
+#say and show desktop notifications
 DISPLAY=:0.0 /usr/bin/notify-send "10 seconds.."
+spd-say "10 seconds..."
 
 #sleep for 10 seconds
 sleep 10
+
+#say and show desktop notifications
+DISPLAY=:0.0 /usr/bin/notify-send "Starting backup"
+spd-say "Starting backup"
 
 #rsync with source and destination
 rsync -r --delete  $source $destination
 
-#show desktop notifications (confirmation)
-DISPLAY=:0.0 /usr/bin/notify-send "Pendrive Synced ✔"
+#say and show desktop notifications (confirmation)
+DISPLAY=:0.0 /usr/bin/notify-send "Backup complete ✔"
+spd-say "Backup complete ✔"
+
