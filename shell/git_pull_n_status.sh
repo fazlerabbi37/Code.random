ba14c1427d83f29fee720cb9cfb9edd0fe746419
Fazle Rabbi <fazlerbbi37@users.noreply.github.com>
2017-04-19 04:26:29 +0600

    add input validation and invalid message in git_pull_n_status.sh

--- shell/git_pull_n_status.sh
+++ shell/git_pull_n_status.sh
@@ -21,11 +21,16 @@ echo "For git pull type without quote 'pull' and for git status 'status':"
 #read pull or status in variable gop
 read gop
 
+#check if gop is pull or status; if not show invalid
+if [ "$gop" == "pull" ] || [ "$gop" == "status" ] ; then
 
-for d in $pro/*/
-#loop through all the sub-directories of Project directories and giv command saved in var gop
-#take a sub-directory and save it in variable d
-do
-    #change drive to the sub-dedicatory saved in variable d and give gop command
-    (cd "$d" && echo $d && git $gop && printf "\n------------------------------------------------------------------------------\n")
-done
+    for d in $pro/*/
+    #loop through all the sub-directories of Project directories and giv command saved in var gop
+    #take a sub-directory and save it in variable d
+    do
+        #change drive to the sub-dedicatory saved in variable d and give gop command
+        (cd "$d" && echo $d && git $gop && printf "\n------------------------------------------------------------------------------\n")
+    done
+else
+    echo -e "\e[31mInvalid input!!! Enter either 'pull' or 'status' without quote.\e[0m"
+fi
