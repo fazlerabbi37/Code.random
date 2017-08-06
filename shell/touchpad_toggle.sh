b340aaeda464e4758d7072c43d5597252c6a2039
Fazle Rabbi <fazlerabbi37@users.noreply.github.com>
2017-08-07 00:35:37 +0600

    add touchpad_toggle, detect_mouse function and argument passing

--- shell/touchpad_toggle.sh
+++ shell/touchpad_toggle.sh
@@ -6,19 +6,63 @@
 
 #!/bin/bash
 
+
 #find touchpad id and save it in variable t
-t=$(xinput list | grep "Touchpad" | awk '{print $6}')
-t=$(echo $t | cut -d'=' -f2)
+touchpad_id=$(xinput list | grep "Touchpad" | awk '{print $6}')
+touchpad_id=$(echo $touchpad_id | cut -d'=' -f2)
+
+#toggles the touchpad
+function touchpad_toggle() { 
+	while read -r line
+	do
+		if_mouse_exists=$(lsusb | grep "$line")
+		#if exist turn off touchpad
+		if [[ ! -z "$if_mouse_exists" ]]
+		then
+    		echo "USB Mouse Detected"
+    		xinput set-prop $touchpad_id "Device Enabled" 0
+		fi
+	done < "/home/$USER/.mouse_name"	
+}
 
 
-#run lsusb and 'see' if "Optical Mouse" is present by using 'grap' command and save it in varable e
-e=$(lsusb | grep "Optical Mouse")
+#function detect_mouse detects USB mouse
+function detect_mouse() {
+	echo "Please plug-out your USB mouse and press enter"
+	sleep 5s
+	read -p "Press Enter to continue"
+	lsusb > without_mouse.txt
+	sleep 5s
+	echo "Please plug-in your USB mouse and press enter"
+	echo "Please don't enter any other USB device other the the mouse"
+	sleep 5s
+	read -p "Press Enter to continue"
+	lsusb > with_mouse.txt
+	sleep 5s
+	comm -23 <(sort with_mouse.txt ) <(sort without_mouse.txt) > mouse.txt
+	read mouse < mouse.txt
+	to_be_deleted_part="$(echo $mouse | awk '{print $1 " " $2 " " $3 " " $4 " " $5 " " $6}')"
+	mouse_name=${mouse#$to_be_deleted_part}
+	echo $mouse_name >> /home/$USER/.mouse_name
+	rm without_mouse.txt with_mouse.txt mouse.txt
+	
+} 
 
-#if exist turn off touchpad
-if [[ ! -z "$e" ]]
+
+if [ "$1" == "-d" ]
 then
-    echo "USB Mouse Detected"
-    xinput set-prop $t "Device Enabled" 0
+	detect_mouse
+	kill $$
+elif [ "$1" == "-h" ]
+then
+	echo "Pass argument -d to detect a USB mouse and -h to show this message"
+	kill $$
+fi
+
+if [ -e "/home/$USER/.mouse_name" ]
+then
+	touchpad_toggle
 else
-    xinput set-prop $t "Device Enabled" 1
+	detect_mouse
+	touchpad_toggle
 fi
