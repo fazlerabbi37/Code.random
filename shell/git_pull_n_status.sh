a4211b0f271afdcdf16f9a345eb37b79fdd8a9f6
Fazle Rabbi <fazlerabbi37@users.noreply.github.com>
2017-08-14 19:43:00 +0600

    add argument passing on git_pull_n_status.sh

--- shell/git_pull_n_status.sh
+++ shell/git_pull_n_status.sh
@@ -11,15 +11,22 @@
 echo "Enter project location:"
 echo "Try not to put / at the end (i.e /home/Projects/)"
 
+
 #read location and save in variable pro
 read pro
 
-#ask if pull or status
-echo "Do you want to pull repo or check status?"
-echo "For git pull type without quote 'pull' and for git status 'status':"
+#check if any argument is passed though command line if not ask for it
+if [ $# -lt 1 ]
+then
+    #ask if pull or status
+    echo "Do you want to pull repo or check status?"
+    echo "For git pull type without quote 'pull' and for git status 'status':"
 
-#read pull or status in variable gop
-read gop
+    #read pull or status in variable gop
+    read gop
+else
+    gop=$1
+fi
 
 #check if gop is pull or status; if not show invalid
 if [ "$gop" == "pull" ] || [ "$gop" == "status" ] ; then
@@ -31,6 +38,7 @@ if [ "$gop" == "pull" ] || [ "$gop" == "status" ] ; then
         #change drive to the sub-dedicatory saved in variable d and give gop command
         echo ""
         echo -e "\e[0;34m-----------------------------------------------------------------------------\e[0m"
+        echo $gop
         (cd "$d" && echo $d && git $gop)
         echo -e "\e[0;34m-----------------------------------------------------------------------------\e[0m"
         echo ""
