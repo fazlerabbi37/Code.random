1d33a2a514d874b4fa03d6d8f5eaf4a930487454
Fazle Rabbi <fazlerabbi37@users.noreply.github.com>
2017-09-21 18:07:34 +0600

    Modified are_you_charging.sh
    I realise that I don't want to know if my battery is charging or not. What I wanted to know that if my charger or power cable is connected
    to the power or not. So you upower is not a great tool for this. Instead I cat the /sys/class/power_supply/BAT0/power_now file which gives
    us a value 0 if the laptop is taking direct AC power thus giving us the information we need.

--- shell/are_you_charging.sh
+++ shell/are_you_charging.sh
@@ -2,20 +2,19 @@
 
 #shell script name: are_you_charging.sh
 
-#the purpose of the shell script is to determine if laptop is charging or not and notifying the user
+#the purpose of the shell script is to determine if laptop is connected to power supply or not and notifying the user
 
 #!/bin/bash
 
 #get the battery status from upower command and save it in variable status
-status=$(upower -i $(upower -e | grep 'BAT') | grep "state" | awk '{print $2}')
+status=$(cat /sys/class/power_supply/BAT0/power_now)
 
 #check if status and notify
-if [[ $status == "charging" ]]
+if [[ $status == "0" ]]
 then
-    echo "charging"
-    DISPLAY=:0.0 /usr/bin/notify-send "charging"
-elif [[ $status == "discharging" ]]
-then
-    echo "discharging"
-    DISPLAY=:0.0 /usr/bin/notify-send "discharging"
+    echo "connected to power supply"
+    DISPLAY=:0.0 /usr/bin/notify-send "connected to power supply"
+else
+    echo "not connected to power supply"
+    DISPLAY=:0.0 /usr/bin/notify-send "not connected to power supply"
 fi
