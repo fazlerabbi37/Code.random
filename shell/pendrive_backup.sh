b787aa5eca99d40bd521137086149a084636976b
Fazle Rabbi <fazlerabbi37@users.noreply.github.com>
2017-10-27 11:28:20 +0600

    Add backup function and source, destination check
    added backup function that gives a 30 seconds countdown and start backup then gives a backup finished notification
    also added checking for source and destination. If source is not found it say to pulgin pendirve and press enter to continue
    if destination is not found makes a destination directory

--- shell/pendrive_backup.sh
+++ shell/pendrive_backup.sh
@@ -14,36 +14,60 @@ read source
 echo "Enter the local drive location:"
 read destination
 
+backup(){
+    #say and show desktop notifications
+    DISPLAY=:0.0 /usr/bin/notify-send "Starting backup within next 30 seconds"
+    spd-say "Starting backup within next 30 seconds"
 
-#say and show desktop notifications
-DISPLAY=:0.0 /usr/bin/notify-send "Starting backup within next 30 seconds"
-spd-say "Starting backup within next 30 seconds"
+    #sleep for 10 seconds
+    sleep 10
 
-#sleep for 10 seconds
-sleep 10
+    #say and show desktop notifications
+    DISPLAY=:0.0 /usr/bin/notify-send "20 seconds..."
+    spd-say "20 seconds..."
 
-#say and show desktop notifications
-DISPLAY=:0.0 /usr/bin/notify-send "20 seconds..."
-spd-say "20 seconds..."
+    #sleep for 10 seconds
+    sleep 10
 
-#sleep for 10 seconds
-sleep 10
+    #say and show desktop notifications
+    DISPLAY=:0.0 /usr/bin/notify-send "10 seconds.."
+    spd-say "10 seconds..."
 
-#say and show desktop notifications
-DISPLAY=:0.0 /usr/bin/notify-send "10 seconds.."
-spd-say "10 seconds..."
+    #sleep for 10 seconds
+    sleep 10
 
-#sleep for 10 seconds
-sleep 10
+    #say and show desktop notifications
+    DISPLAY=:0.0 /usr/bin/notify-send "Starting backup"
+    spd-say "Starting backup"
 
-#say and show desktop notifications
-DISPLAY=:0.0 /usr/bin/notify-send "Starting backup"
-spd-say "Starting backup"
+    #rsync with source and destination
+    rsync -r --delete  $source $destination
 
-#rsync with source and destination
-rsync -r --delete  $source $destination
 
-#say and show desktop notifications (confirmation)
-DISPLAY=:0.0 /usr/bin/notify-send "Backup complete ✔"
-spd-say "Backup complete ✔"
+    #say and show desktop notifications (confirmation)
+    DISPLAY=:0.0 /usr/bin/notify-send "Backup complete ✔"
+    spd-say "Backup complete ✔"
 
+}
+
+if [ ! -d $destination ]
+then
+    echo "Destination directory doesn't exist. Making destination directory.."
+    mkdir -p $destination
+fi
+
+if [ -d $source ]
+then
+    backup
+
+else
+    #say and show desktop notifications
+    DISPLAY=:0.0 /usr/bin/notify-send "Pendrive is not pluged in. Plugin and press enter"
+    spd-say "Pendrive is not pluged in. Plugin and press enter"
+
+    #press enter to continue
+    read -p "Press Enter to continue"
+
+    backup
+
+fi
