e2551d8be9e69fab3a00fd9c64b79e2deebbf1b5
Fazle Rabbi <fazlerabbi37@users.noreply.github.com>
2017-11-14 12:56:48 +0600

    Add remote and branch in git_pull_n_status.sh
    added remote and branch in git_pull_n_status.sh allowing user to choose which remote and branch to pull or see status

--- shell/git_pull_n_status.sh
+++ shell/git_pull_n_status.sh
@@ -24,8 +24,19 @@ then
 
     #read pull or status in variable gop
     read gop
+
+    #ask and read remote name and save it in remote_name
+    read -p "Enter the remote name[Usually it is 'origin']: " remote_name
+    #ask and read local branch name and save it in branch_name
+    read -p "Enter the local branch name[Usually it is 'master']: " branch_name
+
+
+
 else
     gop=$1
+    remote_name=$2
+    branch_name=$3
+
 fi
 
 #check if gop is pull or status; if not show invalid
@@ -38,7 +49,7 @@ if [ "$gop" == "pull" ] || [ "$gop" == "status" ] ; then
         #change drive to the sub-dedicatory saved in variable d and give gop command
         echo ""
         echo -e "\e[0;34m-----------------------------------------------------------------------------\e[0m"
-        (cd "$d" && echo "repo name: $(basename $d)" && git $gop)
+        (cd "$d" && echo "repo name: $(basename $d)" && git $gop $remote_name $branch_name)
         echo -e "\e[0;34m-----------------------------------------------------------------------------\e[0m"
         echo ""
     done
