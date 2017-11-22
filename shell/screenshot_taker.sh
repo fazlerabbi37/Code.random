6ee1aebc515d617f4a356837cf6f47f3c75829a7
Fazle Rabbi <fazlerabbi37@users.noreply.github.com>
2017-11-23 01:50:21 +0600

    Modify screenshot_taker.sh
    modified screenshot_taker.sh ... as I was new to bash I used grep 3 time to get date, month and year that can be achieve with using
    date command once using formatting ... and changed the screen short taking program from gnome-screenshot to import which is also installed
    by default with Ubuntu.

--- shell/screenshot_taker.sh
+++ shell/screenshot_taker.sh
@@ -2,9 +2,10 @@
 
 #screenshot_taker.sh
 
-#the purpose of the shell script to take screenshot using gnome-screenshot after a certain time and save it to a location
+#the purpose of the shell script to take screenshot using import command after a certain time and save it to a location
 
 #!/bin/bash
+
 
 #time interval on each screenshot
 echo "How much time do you want to wait within each screenshot? (in seconds)"
@@ -16,22 +17,13 @@ echo -e "\e[31m'/' after directory like /home/ is not allowed\e[0m"
 read loc
 
 
-#make a folder name in the format of dd_mm_yyyy_hh:mm:ss ie. 10_Jan_2017
-u=_ #take a "_" in variable u
-temp="$(date | awk '{print $3}')" #take dd form date command and save it in variable temp
-dir=$dir$temp$u			  #concat dd saved in variable temp with variable dir
-temp="$(date | awk '{print $2}')" #take mm form date command and save it in variable temp
-dir=$dir$temp$u                   #concat mm saved in variable temp with variable dir
-temp="$(date | awk '{print $6}')" #take yyyy form date command and save it in variable temp
-dir=$dir$temp$u                   #concat yyyy saved in variable temp with variable dir
-temp="$(date | awk '{print $4}')" #take hh:mm:ss form date command and save it in variable temp
-dir=$dir$temp			  #concat hh:mm:ss saved in variable temp with variable dir
+#make a folder name in the format of dd-mm-yyyy ie. 10-Jan-2017
+#get the current date form date command
+current_date=$(date +"%d-%b-%Y")
 
-#make a directory in the location saved in loc variable with a format saved in variable dir
-mkdir $loc/$dir
+#make a directory in the location with the value saved in current_date variable
+mkdir -p $loc/$current_date
 
-#take variable n with a value 1 that will increase with each screenshot taken 
-n=1
 
 #echo a message
 echo "screenshot_taker is in action. Press Ctrl+c to stop it."
@@ -39,8 +31,7 @@ echo "screenshot_taker is in action. Press Ctrl+c to stop it."
 #infinite while loop
 while :
 do
-    gnome-screenshot -f $loc/$dir/ss$n.png #take screenshot and save it in location
-    echo "Number of screenshot taken: $n" #show screenshot counter
-    n=$(( n+1 ))        #increase the value of n
+    #take screenshot and save it in location with current time
+    import -window root $loc/$current_date/ss_$(date +"%T").png
     sleep $delay_time   #sleep for $delay_time second(s)
 done
