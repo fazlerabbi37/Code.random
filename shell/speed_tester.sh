e181024c7b7dca88efd3c6cb4b3fb3eafc04851b
Fazle Rabbi <fazlerabbi37@gmail.com>
2018-10-24 17:36:08 +0600

    Modify speed_tester.sh to download file in tmp

--- shell/speed_tester.sh
+++ shell/speed_tester.sh
@@ -12,7 +12,7 @@ echo "downloading a standard 5MB zip file..."
 
 #download a file form thinkbroadband.com and find out its download time with time command 
 #save the real time value and save it on down_time variable
-down_time=`/usr/bin/time -f %e wget -q http://download.thinkbroadband.com/5MB.zip 2>&1`
+down_time=`/usr/bin/time -f %e wget -q -o /tmp/5MB.zip http://download.thinkbroadband.com/5MB.zip 2>&1`
 
 #save the integer part of down_time on variable time_sec
 time_sec="$(echo $down_time | awk -F. '{print $1}')"
@@ -27,5 +27,5 @@ echo "it took $down_time seconds to download the file at an average speed of $kb
 DISPLAY=:0.0 /usr/bin/notify-send "t: $down_time s; s: $kbps kbps"
 
 #remove downloaded file
-rm 5MB.zip
+rm /tmp/5MB.zip
 
